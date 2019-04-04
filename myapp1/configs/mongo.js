fded872b8b845b01329dbe0b648d595b72456f7d
Andrii Petrov <apetrov010@cee.ema.ad.pwcinternal.com>
2019-04-04 19:02:27 +0300

    mongo db connect

--- myapp1/configs/mongo.js
+++ myapp1/configs/mongo.js
@@ -1,14 +1,23 @@
 var mongoose = require('mongoose');
 
 //const dbURI = 'mongodb://ds115533.mlab.com:15533/films_db';
-const dbURI = 'mongodb+srv://Bratok3000:p0o9i8u7@cluster0-stcrx.mongodb.net/test?retryWrites=true';
-var dbOptions = {
-    user: 'filmapp',
-    pass: 'NtqfEw2r',
-    useNewUrlParser: true
-};
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://Bratok3000:$p0o9i8u7@cluster0-stcrx.mongodb.net/test?retryWrites=true";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
-mongoose.connect(dbURI, dbOptions);
+const dbURI = 'mongodb://Bratok3000:%36p0o9i8u7@cluster0-stcrx.mongodb.net/test?retryWrites=true';
+// var dbOptions = {
+//     user: 'Andrii',
+//     pass: '$p0o9i8u7',
+//     useNewUrlParser: true
+// };
+
+mongoose.connect(dbURI);
 
 mongoose.connection.on('connected', function() {
     console.info("Mongoose connected to: " + dbURI); 
