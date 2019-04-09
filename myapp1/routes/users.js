dd63e647a4fa56e9ba0a4c94549276d299d8244f
Andrii Petrov <apetrov010@cee.ema.ad.pwcinternal.com>
2019-04-09 18:18:33 +0300

    some of the changes

--- myapp1/routes/users.js
+++ myapp1/routes/users.js
@@ -1,4 +1,43 @@
 var express = require('express');
+var jwt = require('jsonwebtoken');
+var User = require('../models/user.model');
+
+var router = express.Router();
+
+router.post('/signup', function(req, res) {
+  if(!req.body.username || !req.body.password) {
+    res.json({success: false, msg: 'Pleace pass username and password'});
+  } else {
+    var newUser = new User(req.body);
+
+    newUser.save(function(err) {
+      if(err) {
+        return res.json({success: true, msg: 'Username already exists.'});
+      }
+      res.json({success: true, msg: 'Successful created new user.'});
+    });
+  }
+});
+
+router.post('/signin', function(req, res) {
+  User.findOne({username: req.body.username}, function(err, user) {
+    if(err) throw true;
+    
+    if(!user) {
+      return res.status(401).send({success: false, msg: 'Authentication failed. User not found'});
+    }
+
+    user.comparePassword(req.body.password, function(err, isMatch) {
+      if(isMatch && !err) {
+        var token = jwt.sign(user.toObject(), 'secret-word', {expiresIn: 'id'});
+        return res.status(200).json({success: true, token: 'JWT' + token});
+      }
+      res.status(401).send({success: false, msg: 'Authentafication failed. Wrong password.'});
+    })
+  })
+})
+
+
 var router = express.Router();
 
 /* GET users listing. */
